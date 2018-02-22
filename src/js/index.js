61484d0c5542de65ed36d0424f9d9dd8eddc9a9b
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2018-02-21 19:51:07 -0600

    added new icons, mobile menu begining and minor style changes

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,28 @@ $(window).scroll(() =>{
     $('header').removeClass('blue')
   }
 });
+$('#toggleMenu').on('click', ev =>{
+  ev.preventDefault();
+  var $iconMenu = $('#toggleMenu').find('span');
+  var isOpening = $iconMenu.attr('class') == 'icon-menu';
+  $iconMenu.removeClass();
+  if (isOpening) {
+    $iconMenu.addClass('icon-close');
+    $('.mobile-menu').css({
+      "width": "100%"
+    });
+    $('body').css({
+      "overflow-y": "hidden"
+    });
+  }
+  else{
+    $iconMenu.addClass('icon-menu');
+    $('.mobile-menu').css({
+      "width": "0%"
+    });
+    $('body').css({
+      "overflow-y": "initial"
+    });
+  }
+
+});
